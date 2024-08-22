d796bc6b6801d19877c2911e8a79d48e910ec6d9
Fernando Garcia <fernando.d.garcia@gmail.com>
2024-08-22 12:14:46 -0300

    docker login added

--- docker_push.sh
+++ docker_push.sh
@@ -1,9 +1,13 @@
+echo "docker login"
+docker login
+
+
 TAG="1.03" 
 echo "Building..."
 docker build -t practicasdedesarrollounq/go_webserver:$TAG .
 
-echo "listing images"
-docker images
+#echo "listing images"
+#docker images
 
 echo "pushing... tag $TAG"
 docker push practicasdedesarrollounq/go_webserver:$TAG 
